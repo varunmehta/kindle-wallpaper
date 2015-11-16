9fa14d32d12caf2c94429690cee76b9a7ecfdd26
Varun Mehta <varun.mehta.r@gmail.com>
2015-11-15 23:41:18 -0500

    Weather code complete, now time to write the kindle pull code.

--- In the Kindle/launch.sh
+++ In the Kindle/launch.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 rm /mnt/us/screensaver/done.png
-wget -O "/mnt/us/screensaver/done.png" "http://path to your server/kindle/done.png"
+wget -O "/mnt/us/screensaver/done.png" "http://raspberrypi/kindle/done.png"
 reboot
